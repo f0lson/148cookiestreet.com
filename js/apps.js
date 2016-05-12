5b3f83b8b9f75d23f3d519fd647912cc3aaf8f46
Forrest Olson <forrest@odotmedia.com>
2016-05-12 12:22:48 -0500

    Re-Uploaded Correct Files
    New stylesheet, more imgs, updated apps.js, updated index file

--- js/apps.js
+++ js/apps.js
@@ -1,22 +1,29 @@
 //Make sure the document is fully loaded
 $(document).ready(function() {
-    
+    // Create Variables
     var $menu = $('#menu');
     var $iNav = $('#i-nav');
     
+    //Click on the menu icon aka $iNav
     $iNav.click( function(event) {
+        //Test if $menu is :hidden
         if( $('#menu').is(':hidden') ) {
+            //If $menu is :hidden, show it.
             $('#menu').show('fast');
-        } else {
+        } 
+            //If menu is showing, hide it.
+            else {
             $('#menu').hide('fast');
         }
         event.preventDefault();
     } );
     
-//    $(window).resize(function() {
-//        if( $(window).width() < 825 ){
-//                    
-//        }
-//    });
+    //Responsive menu
+    if ( $(window).width() < 754 ) {
+        $menu.css('display', 'flex');
+        
+    } else {
+        // do nothing //
+    }
     
 });
